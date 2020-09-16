9535e5beb8b06e03cf88eea43b089aaeb6ab4df7
ravi yadav <raviyadav00063@gmail.com>
2020-09-16 19:56:22 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCFVoP-KjEwmFkDlDM3KJrwmm0oBWGi9Sk",
+  authDomain: "whatsapp-mern-2f30c.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-2f30c.firebaseio.com",
+  projectId: "whatsapp-mern-2f30c",
+  storageBucket: "whatsapp-mern-2f30c.appspot.com",
+  messagingSenderId: "690635351882",
+  appId: "1:690635351882:web:0abbb398f04a50e6e1c3b3",
+};
