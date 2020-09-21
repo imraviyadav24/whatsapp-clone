8614a81ca7c6bf4ae87f835d410b048134ff56d8
ravi yadav <raviyadav00063@gmail.com>
2020-09-21 22:18:42 +0530

    Frontend done

--- src/Chat.js
+++ src/Chat.js
@@ -1,6 +1,8 @@
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import { Avatar, IconButton } from "@material-ui/core";
 import React from "react";
+import InsertEmotionIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 import "./Chat.css";
 
 function Chat() {
@@ -25,6 +27,29 @@ function Chat() {
             <MoreVert />
           </IconButton>
         </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Ravi</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Ravi</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmotionIcon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
